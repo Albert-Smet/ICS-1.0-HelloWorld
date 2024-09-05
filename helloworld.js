34ff5491fd19a048887c1488c65b08b0512f1daa
Albert <1smetanaalb@hdsb.ca>
2024-09-05 16:34:35 +0000

    Added a "Hello World" Message.

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,7 @@
 // Log "Hello World" to the console.
 // Log four additional messages of your choosing. 
 // Comment each message appropriately. 
+console.log("Hello World");
+console.log("Choom");
+console.log("The Chorg");
+console.log("Con Cord");
